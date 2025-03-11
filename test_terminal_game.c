8dec01cecc2b30c9a0f01ac9412b63bfde82d3c1
Haozhe Li <lihaozhe013@outlook.com>
2025-03-11 19:49:30 -0400

    new

--- test_terminal_game.c
+++ test_terminal_game.c
@@ -1,138 +1,75 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <termios.h>
-#include <fcntl.h>
-#include <time.h>
+// clang test_terminal_game.c -lncurses
+#include <ncurses.h>
 
-#define WIDTH 20
-#define HEIGHT 10
+// Define colors
+#define PLAYER_COLOR 1
+#define BG_COLOR 2
 
-// 方向控制
-enum Direction { UP, DOWN, LEFT, RIGHT };
-struct Snake {
-    int x, y;
-} snake[100];
+// Define player starting position
+int player_x = 10, player_y = 5;
+int start_x = 10, start_y = 5;
 
-int length = 5;
-enum Direction dir = RIGHT;
-int food_x, food_y;
-
-// **设置终端为非阻塞模式**
-void setup_terminal() {
-    struct termios t;
-    tcgetattr(STDIN_FILENO, &t);
-    t.c_lflag &= ~(ICANON | ECHO); // 关闭缓冲 & 关闭回显
-    tcsetattr(STDIN_FILENO, TCSANOW, &t);
+void draw_block(int y, int x, chtype ch, int color_pair) {
+    attron(COLOR_PAIR(color_pair));
+    mvaddch(y, x, ch);
+    attroff(COLOR_PAIR(color_pair));
 }
 
-// **检查键盘输入（非阻塞）**
-int kbhit() {
-    struct termios oldt, newt;
+void game_loop() {
     int ch;
-    int oldf;
-
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-
-    ch = getchar();
-
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-    if (ch != EOF) {
-        ungetc(ch, stdin);
-        return 1;
-    }
-    return 0;
-}
-
-// **渲染游戏界面**
-void draw() {
-    system("clear"); // 清屏
-    for (int i = 0; i <= HEIGHT; i++) {
-        for (int j = 0; j <= WIDTH; j++) {
-            if (i == 0 || i == HEIGHT || j == 0 || j == WIDTH) {
-                printf("#"); // 边界
-            } else if (i == food_y && j == food_x) {
-                printf("*"); // 食物
-            } else {
-                int isBody = 0;
-                for (int k = 0; k < length; k++) {
-                    if (snake[k].x == j && snake[k].y == i) {
-                        printf("O"); // 蛇身
-                        isBody = 1;
-                        break;
-                    }
-                }
-                if (!isBody) printf(" ");
+    while (1) {
+        clear();
+        
+        // Draw background blocks
+        for (int i = 0; i < LINES; i++) {
+            for (int j = 0; j < COLS; j++) {
+                draw_block(i, j, ' ', BG_COLOR);
             }
         }
-        printf("\n");
-    }
-}
 
-// **更新游戏逻辑**
-void update() {
-    // 处理输入
-    if (kbhit()) {
-        char key = getchar();
-        switch (key) {
-            case 'w': if (dir != DOWN) dir = UP; break;
-            case 's': if (dir != UP) dir = DOWN; break;
-            case 'a': if (dir != RIGHT) dir = LEFT; break;
-            case 'd': if (dir != LEFT) dir = RIGHT; break;
-            case 'q': exit(0); // 退出
+        // Draw player block
+        draw_block(player_y, player_x, '@', PLAYER_COLOR);
+
+        refresh();
+        
+        ch = getch(); // Get user input
+        
+        switch (ch) {
+            case KEY_UP:
+                if (player_y > 0) player_y--;
+                break;
+            case KEY_DOWN:
+                if (player_y < LINES - 1) player_y++;
+                break;
+            case KEY_LEFT:
+                if (player_x > 0) player_x--;
+                break;
+            case KEY_RIGHT:
+                if (player_x < COLS - 1) player_x++;
+                break;
+            case 10:  // Enter key (Reset position)
+                player_x = start_x;
+                player_y = start_y;
+                break;
+            case 'q': // Quit game
+                return;
         }
     }
-
-    // 移动蛇
-    for (int i = length - 1; i > 0; i--) {
-        snake[i] = snake[i - 1];
-    }
-    if (dir == UP) snake[0].y--;
-    if (dir == DOWN) snake[0].y++;
-    if (dir == LEFT) snake[0].x--;
-    if (dir == RIGHT) snake[0].x++;
-
-    // 碰撞检测
-    if (snake[0].x == 0 || snake[0].x == WIDTH || snake[0].y == 0 || snake[0].y == HEIGHT) {
-        printf("Game Over!\n");
-        exit(0);
-    }
-
-    // 食物检测
-    if (snake[0].x == food_x && snake[0].y == food_y) {
-        length++;
-        food_x = rand() % (WIDTH - 1) + 1;
-        food_y = rand() % (HEIGHT - 1) + 1;
-    }
 }
 
-// **主循环**
-int main(void) {
-    srand(time(NULL));
-    setup_terminal();
-    
-    // 初始化蛇
-    for (int i = 0; i < length; i++) {
-        snake[i].x = 5 - i;
-        snake[i].y = 5;
-    }
-    
-    // 生成食物
-    food_x = rand() % (WIDTH - 1) + 1;
-    food_y = rand() % (HEIGHT - 1) + 1;
+int main() {
+    initscr();            // Initialize ncurses mode
+    start_color();        // Enable color support
+    keypad(stdscr, TRUE); // Enable arrow keys
+    noecho();             // Don't echo keypresses
+    curs_set(0);          // Hide cursor
 
-    while (1) {
-        draw();
-        update();
-        usleep(200000); // 控制游戏速度 (200ms)
-    }
+    // Define color pairs
+    init_pair(PLAYER_COLOR, COLOR_RED, COLOR_BLACK);
+    init_pair(BG_COLOR, COLOR_BLUE, COLOR_BLUE);
 
+    game_loop(); // Start the game loop
+
+    endwin(); // Restore normal terminal mode
     return 0;
 }
