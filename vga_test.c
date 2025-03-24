a5fc3613998a1cc235622a11c8c87e4749a601a0
wilbertbw <wilbertbwibowo@gmail.com>
2025-03-24 17:42:17 -0400

    added keyboard input (works) using polling

--- vga_test.c
+++ vga_test.c
@@ -3,7 +3,6 @@
 #include <time.h>
 #define HALF_WIDTH_BOX 2
 #define PLAYER_SPEED 5
-#define player_pos_y 220
 #define SCREEN_HEIGHT 240
 #define SCREEN_WIDTH 320
 #define PLYAER_X_OFFSET 3
@@ -15,11 +14,13 @@ short int Buffer2[240][512];
 
 
 int player_pos_x = SCREEN_WIDTH / 2;
-int old_player_pos_x;
+int player_pos_y = 220;
+int old_player_pos_x, old_player_pos_y;
 int obstacles_pos[3][2], obstacle_height[3], obstacle_width[3];
 int obstacles_old_pos[3][2], obstacle_old_height[3], obstacle_old_width[3];
 int current_speed = 5;
 int arrow_input = 0;
+char byte1 = 0, byte2 = 0;
 
 void swap(int *a, int *b);
 void plot_pixel(int x, int y, short int line_color);
@@ -30,17 +31,20 @@ void resetObstacle(int pos[2], int *height, int *width, int idx);
 void resetObstacle2(int pos[2], int *height, int *width, int idx);
 void get_button_input();
 void get_keyboard_input();
+void get_keyboard_input_poll();
+void handler(void);
 
 int main(void)
 {
     arrow_input = 0;
 	srand(time(NULL));
     volatile int * pixel_ctrl_ptr = (int *)0xFF203020;
+    volatile int * keyboard_ptr = (int * )0xFF200100;
     // declare other variables(not shown)
     // initialize location and direction of rectangles(not shown)
 
     /* set front pixel buffer to Buffer 1 */
-    *(pixel_ctrl_ptr + 1) = (int) &Buffer1; // first store the address in the  back buffer
+    *(pixel_ctrl_ptr + 1) = (int) &Buffer1;
     /* now, swap the front/back buffers, to set the front buffer location */
     wait_for_vsync();
     /* initialize a pointer to the pixel buffer, used by drawing functions */
@@ -51,9 +55,31 @@ int main(void)
     *(pixel_ctrl_ptr + 1) = (int) &Buffer2;
     pixel_buffer_start = *(pixel_ctrl_ptr + 1); // we draw on the back buffer
     clear_screen(); // pixel_buffer_start points to the pixel buffer
+
+    // processor side interrupt setup
+    int mstatus, mtvec, mie;
+
+    mstatus = 0x8;
+    __asm__ volatile("csrc mstatus, %0" :: "r"(mstatus));
+
+    mtvec = (int)&handler;
+    __asm__ volatile("csrw mtvec, %0" :: "r"(mtvec));
+
+    __asm__ volatile("csrr %0, mie" : "=r"(mie));
+    __asm__ volatile("csrc mie, %0" :: "r"(mie));
+
+    mie = 0x400000;
+    __asm__ volatile("csrs mie, %0" :: "r"(mie));
+
+    __asm__ volatile("csrs   mstatus, %0" :: "r"(mstatus));
+
+    // keyboard side interrupt setup
+
+    // *(keyboard_ptr + 1) = 0b1; // enable interrupts from keyboard
    
     // draw player for the first time
     old_player_pos_x = player_pos_x;
+    old_player_pos_y = player_pos_y;
     draw_ranctangle(player_pos_x, player_pos_y, PLYAER_X_OFFSET, PLAYER_Y_OFFSET, 0xFFFF);
    
     // draw the obstacles for the first time
@@ -68,9 +94,10 @@ int main(void)
     }
    
     for (;;) {
+        get_keyboard_input_poll();
         // ===================clear screen====================
         // clear player
-        draw_ranctangle(old_player_pos_x, player_pos_y, PLYAER_X_OFFSET + PLAYER_SPEED, PLAYER_Y_OFFSET, 0);
+        draw_ranctangle(old_player_pos_x, old_player_pos_y, PLYAER_X_OFFSET + PLAYER_SPEED, PLAYER_Y_OFFSET + PLAYER_SPEED, 0);
        
         // clear obstacles
         for (int i = 0; i < 3; ++i) {
@@ -78,6 +105,7 @@ int main(void)
         }
         // ================end if clear screen================
 
+        get_keyboard_input_poll();
         // ================draw new elements==================
         // draw new player
         draw_ranctangle(player_pos_x, player_pos_y, PLYAER_X_OFFSET, PLAYER_Y_OFFSET, 0xFFFF);
@@ -89,6 +117,7 @@ int main(void)
         // Wait for vertical sync to swap buffers
         // =============end of draw new elements==============
 
+        get_keyboard_input_poll();
         wait_for_vsync();
        
         // Update the pixel_buffer_start pointer to the new back buffer
@@ -96,9 +125,10 @@ int main(void)
        
 
 
-
+        get_keyboard_input_poll();
         // ==========Update the previous position===========
         old_player_pos_x = player_pos_x;
+        old_player_pos_y = player_pos_y;
         for (int i = 0; i < 3; ++i) {
             obstacles_old_pos[i][0] = obstacles_pos[i][0];
             obstacles_old_pos[i][1] = obstacles_pos[i][1];
@@ -114,6 +144,8 @@ int main(void)
         // ===========update elements position=============
         // update player position
         get_button_input();
+        get_keyboard_input_poll();
+        // get_keyboard_input();
         switch (arrow_input)
         {
         case 1:
@@ -124,7 +156,16 @@ int main(void)
             player_pos_x -= PLAYER_SPEED;
             arrow_input = 0;
             break;
+        case 3:
+            player_pos_y -= PLAYER_SPEED;
+            arrow_input = 0;
+            break;
+        case 4:
+            player_pos_y += PLAYER_SPEED;
+            arrow_input = 0;
+            break;
         }
+        get_keyboard_input_poll();
         // update obstacle postion
         for (int i = 0; i < 3; ++i) {
             if (obstacles_pos[i][1] - obstacle_height[i] - 1 >= SCREEN_HEIGHT) {
@@ -134,6 +175,7 @@ int main(void)
             }
         }
         // ========end ofupdate elements position==========
+        get_keyboard_input_poll();
     }
 
 }
@@ -191,31 +233,63 @@ void get_button_input() {
     }
 }
 
+void get_keyboard_input_poll() {
+    volatile int * keyboard_ptr = (int * )0xFF200100;
+    int keyboard_data = *(keyboard_ptr); // read the Data register in the PS/2 port
+    int RVALID = keyboard_data & 0x8000; // extract the RVALID field
+    
+    if (RVALID) {
+        /* shift the next data byte into the display */
+        byte1 = byte2;
+        byte2 = keyboard_data & 0xFF;
+    
+        if ((byte1 == (char)0xE0) && (byte2 == (char)0x74)) { // right arrow key press
+            arrow_input = 1;
+        } else if ((byte1 == (char)0xE0) && (byte2 == (char)0x6B)) { // left arrow key press
+            arrow_input = 2;
+        } else if ((byte1 == (char)0xE0) && (byte2 == (char)0x75)) { // up arrow key press
+            arrow_input = 3;
+        } else if ((byte1 == (char)0xE0) && (byte2 == (char)0x72)) { // down arrow key press
+            arrow_input = 4;
+        }
+    }
+}
+
 void get_keyboard_input() {
     volatile int * keyboard_ptr = (int * )0xFF200100;
-    int input[3] = {0};
-    int inputIndex = 2;
-   
-    if (*(keyboard_ptr) & 0xF) {
-        for (int i = 0; i < (*(keyboard_ptr) & 0xFFFF0000); i++) {
-            input[inputIndex] = *(keyboard_ptr) & 0x7; // save the input
-            inputIndex--;
-            if (inputIndex == -1) { // reset index
-                inputIndex = 2;
-            }
-            if (input[0] == 0xE0 && input[1] == 0xF0 && input[2] == 0x6B) { // left arrow key release
-                arrow_input = 1;
-                break;
-            } else if (input[0] == 0xE0 && input[1] == 0xF0 && input[2] == 0x74) { // right arrow key release
-                arrow_input = 2;
-                break;
-            } else if (input[0] == 0xE0 && input[1] == 0xF0 && input[2] == 0x75) { // up arrow key release
-                arrow_input = 3;
-                break;
-            } else if (input[0] == 0xE0 && input[1] == 0xF0 && input[2] == 0x72) { // down arrow key release
-                arrow_input = 4;
-                break;
-            }
+    int keyboard_data = *(keyboard_ptr);
+    int RAVAIL = keyboard_data & 0x8000;
+
+    printf("%c", RAVAIL);
+
+    while (RAVAIL > 0) {      
+        byte1 = byte2;
+        byte2 = keyboard_data & 0xFF;
+
+        if ((byte2 == (char)0XE0)) {
+            printf("got the first input");
         }
+
+        if ((byte1 == (char)0xE0) && (byte2 == (char)0X6B)) { // left arrow key press
+            arrow_input = 1;
+        } else if ((byte1 == (char)0xE0) && (byte2 == (char)0x74)) { // right arrow key press
+            arrow_input = 2;
+        } else if ((byte1 == (char)0xE0) && (byte2 == (char)0x75)) { // up arrow key press
+            arrow_input = 3;
+        } else if ((byte1 == (char)0xE0) && (byte2 == (char)0x72)) { // down arrow key press
+            arrow_input = 4;
+        }
+        RAVAIL--;
+    }
+}
+
+void handler(void) { // interrupt call does not work
+    int mcause_value;
+    __asm__ volatile ("csrr %0, mcause" : "=r"(mcause_value));
+    printf("interrupt call worked, before if statement\n");
+    // check for the interrupt causer here (check)
+    if (mcause_value ==   0x80000016) { // check if keyboard intterupt
+        printf("interrupt call worked\n");
+        get_keyboard_input();
     }
 }
